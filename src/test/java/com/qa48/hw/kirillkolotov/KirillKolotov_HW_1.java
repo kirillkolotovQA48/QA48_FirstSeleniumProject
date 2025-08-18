3c2d5265db347fd2115abe5aaf60753b83a66812
kirillkolotov <2146610@mail.ru>
2025-08-18 23:23:03 +0300

    Add9

--- src/test/java/com/qa48/hw/kirillkolotov/KirillKolotov_HW_1.java
+++ src/test/java/com/qa48/hw/kirillkolotov/KirillKolotov_HW_1.java
@@ -18,5 +18,5 @@ public class KirillKolotov_HW_1 {
     public void openURL(){
         System.out.println("URL Opened");
     }
-    // проверка push 9
+    // проверка push 7889
 }
---
+++ src/test/java/com/qa48/hw/kirillkolotov/KirillKolotov_HW_2.java
@@ -0,0 +1,4 @@
+package com.qa48.hw.kirillkolotov;
+
+public class KirillKolotov_HW_2 {
+}
